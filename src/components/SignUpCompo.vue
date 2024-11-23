e164b2b571749e22c28ad6b3bffe1169060a8167
markuslem <markus.lemberg@ut.ee>
2024-11-23 17:44:03 +0200

    sing up box created

--- src/components/SignUpCompo.vue
+++ src/components/SignUpCompo.vue
@@ -1,3 +1,30 @@
 <template>
-    <h>Sign up card content</h>
+    <div id="sign-up-card">
+        <p>Email</p>
+        <input type="text" id="email-input" name="email-input">
+        <p>Password</p>
+    </div>
 </template>
+
+<script>
+export default {
+    name: 'SignUpCard'
+}
+</script>
+
+<style scoped>
+#sign-up-card {
+    display: flex;
+    
+    justify-content: center;
+    align-items: center;
+    max-width: 50%;
+    margin: auto;
+    max-width: 50%;
+    row-gap: 20px;
+    column-gap: 10px;
+    border: 4px solid black;
+    border-radius: 15px;
+    background-color: lightblue;
+}
+</style>
